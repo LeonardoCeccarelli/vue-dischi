65c0481c656c5938220fa633fa2f6cbc99641b9d
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-15 16:39:31 +0100

    completed bonus

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -1,28 +1,37 @@
 <template>
-  <div class="container">
-    <div class="row row-cols-1 row-cols-md-5 gx-4 gy-3 my-5">
-      <SingleCard
-        v-for="(song, i) in songList"
-        :key="i"
-        :url="song.poster"
-        :title="song.title"
-        :year="song.year"
-        :genre="song.genre"
-        :author="song.author"
-      ></SingleCard>
+  <section>
+    <div class="container">
+      <div class="row row-cols-1 row-cols-md-5 gx-4 gy-3 my-5">
+        <SingleCard
+          v-for="(song, i) in songList"
+          :key="i"
+          :url="song.poster"
+          :title="song.title"
+          :year="song.year"
+          :genre="song.genre"
+          :author="song.author"
+        ></SingleCard>
+      </div>
     </div>
-  </div>
+
+    <Loader :text="'Caricamento. . .'" v-if="checkLoader"></Loader>
+  </section>
 </template>
 
 <script>
 import axios from "axios";
 import SingleCard from "./SingleCard.vue";
+import Loader from "./Loader.vue";
 export default {
   name: "CardContainer",
-  components: { SingleCard },
+  components: {
+    SingleCard,
+    Loader,
+  },
   data() {
     return {
       songList: [],
+      checkLoader: true,
     };
   },
   mounted() {
@@ -30,7 +39,10 @@ export default {
       .get("https://flynn.boolean.careers/exercises/api/array/music")
       .then((resp) => {
         this.songList = resp.data.response;
-        console.log(this.songList);
+
+        setTimeout(() => {
+          this.checkLoader = false;
+        }, 250);
       });
   },
 };
---
+++ src/components/Loader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="loader_container">
+      <div class="icon_loader spinner-border text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+      <span class="text_loader text-secondary">{{ text }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loader",
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style>
+</style>
