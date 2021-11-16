dba4efcb065af1f11d841120b5ed3ad0e350dd98
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-16 15:00:35 +0100

    add filter with select component

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -1,9 +1,21 @@
 <template>
   <section>
     <div class="container">
-      <div class="row row-cols-1 row-cols-md-5 gx-4 gy-3 my-5">
+      <SelectFilter
+        :songArray="this.songList"
+        @onChangeSelect="getFilteredArray"
+      ></SelectFilter>
+      <div
+        class="
+          row row-cols-1 row-cols-md-5
+          gx-4
+          gy-3
+          my-4
+          justify-content-center
+        "
+      >
         <SingleCard
-          v-for="(song, i) in songList"
+          v-for="(song, i) in filteredSongList"
           :key="i"
           :url="song.poster"
           :title="song.title"
@@ -22,23 +34,40 @@
 import axios from "axios";
 import SingleCard from "./SingleCard.vue";
 import Loader from "./Loader.vue";
+import SelectFilter from "./SelectFilter.vue";
+
 export default {
   name: "CardContainer",
   components: {
     SingleCard,
     Loader,
+    SelectFilter,
   },
   data() {
     return {
       songList: [],
+      filteredSongList: [],
       checkLoader: true,
     };
+  },
+  methods: {
+    getFilteredArray(genre) {
+      if (genre === "Tutti") {
+        this.filteredSongList = this.songList;
+        return this.songList;
+      }
+
+      this.filteredSongList = this.songList.filter((song) => {
+        return song.genre === genre;
+      });
+    },
   },
   mounted() {
     axios
       .get("https://flynn.boolean.careers/exercises/api/array/music")
       .then((resp) => {
         this.songList = resp.data.response;
+        this.filteredSongList = this.songList;
 
         setTimeout(() => {
           this.checkLoader = false;
---
+++ src/components/SelectFilter.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="select_container text-center m-auto w-25 mt-5">
+    <h5 class="">Filtra per <strong>Genere</strong></h5>
+    <select
+      v-model="filterValue"
+      @change="onSelectFilterChange"
+      class="form-select"
+    >
+      <option value="Tutti" selected>Tutti</option>
+      <option v-for="genre in filteredByGenre" :key="genre" :value="genre">
+        {{ genre }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SelectFilter",
+  props: {
+    songArray: Array,
+  },
+  data() {
+    return {
+      filterValue: "Tutti",
+    };
+  },
+  methods: {
+    onSelectFilterChange() {
+      this.$emit("onChangeSelect", this.filterValue);
+    },
+  },
+  computed: {
+    filteredByGenre() {
+      const genresList = [];
+      this.songArray.forEach((el) => {
+        const { genre } = el;
+
+        if (!genresList.includes(genre)) {
+          genresList.push(genre);
+        }
+      });
+
+      return genresList;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
